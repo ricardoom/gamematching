9893797b58b32d467ad44b22166642f197529454
Ricardo E. Galvez <galvez667@gmail.com>
2020-01-21 12:16:13 -0500

    :meat_on_bone: initial commit... doing the things to make a pure JS / HTML / CSS matching / memory game for a learning project

---
+++ app/index.js
@@ -0,0 +1,31 @@
+//
+// a matching game
+//
+
+// TODO: set up things
+// TODO: Easy, set up an html page
+// TODO: more things...
+//
+
+//
+// Draw the cards
+//
+
+const card = {
+  color: 'blue',
+  icon: function cardIcon() {
+    console.log(this.length);
+  },
+};
+
+function singleCard() {
+  //a get some info from the card object
+  card.cardIcon();
+}
+//
+// game logic
+//
+
+//
+// generate the grid
+//

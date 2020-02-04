e2f7b1a24b0162277919f0a6badacbbe9ea2c8b8
Ricardo E. Galvez <galvez667@gmail.com>
2020-02-04 10:18:44 -0500

    :meat_on_bone: some progress but mostly futxing

--- app/cardLogic.js
+++ app/cardLogic.js
@@ -27,7 +27,6 @@ const theCardArray = [
 
 // this shuffle array function came from stack overflow.
 // this version does not mutate the original array
-
 const shuffleArray = function(array) {
   const a = array.slice();
   for (let i = a.length - 1; i > 0; i--) {
@@ -48,10 +47,11 @@ const theShuffledArray = shuffleArray(theCardArray);
 const theCardArrayLength = theCardArray.length;
 
 //
+// stub out the cards
 //
 const cardContainer = document.getElementsByClassName('card');
 
-const dataCardName = 'card-name-';
+const dataCardName = '';
 
 const dataAttribute = 'data-card-name';
 
@@ -73,24 +73,43 @@ const singleCardInner = `
 // create the cards
 //
 const cardMap = theShuffledArray.map(newCard => {
-  const singleCardParentHTMLFragment = document.createRange().createContextualFragment(singleCardHTML);
+  // const singleCardParentHTMLFragment = document.createRange().createContextualFragment(singleCardHTML);
 
   const singleCardInnerHTMLFragment = document.createRange().createContextualFragment(singleCardInner);
 
-  const card = {};
-  (card.face = newCard),
-    (card.id = `${newCard}-${getRandomInt(theCardArrayLength)}`),
-    (card.data = `${dataAttribute}`),
-    (card.html = singleCardHTML),
-    (card.innerHTML = singleCardInnerHTMLFragment),
-    (card.check = function(input) {
+  const otherCard = {
+    face: newCard,
+    id: `${newCard}-${getRandomInt(theCardArrayLength)}`,
+    html: singleCardHTML,
+    innerHTML: singleCardInnerHTMLFragment,
+    check: function(input) {
       if (input !== this.face) {
         console.log(`${input} does not match ${this.face}`);
       } else {
         console.log(`its a match: ${input} == ${this.face}`);
       }
-    });
-  return card;
+    },
+  };
+
+  // console.log(oneCard.check('meteor'));
+
+  // const card = {};
+  // (card.face = newCard),
+  //   (card.id = `${newCard}-${getRandomInt(theCardArrayLength)}`),
+  //   (card.html = singleCardHTML),
+  //   (card.innerHTML = singleCardInnerHTMLFragment),
+  //   (card.check = function(input) {
+  //     if (input !== this.face) {
+  //       console.log(`${input} does not match ${this.face}`);
+  //     } else {
+  //       console.log(`its a match: ${input} == ${this.face}`);
+  //     }
+  //   });
+
+  // return card;
+  const oneCard = Object.create(otherCard);
+  console.log(oneCard.html);
+  return oneCard;
 });
 
 //
@@ -99,7 +118,6 @@ const cardMap = theShuffledArray.map(newCard => {
 
 // console.log(cardMap[3].id);
 // console.log(cardMap[3].face);
-// console.log(cardMap[3].check('meteor'));
 
 //
 // populate boards logic
@@ -108,25 +126,42 @@ const numberOfCards = theCardArray.length;
 
 const board = document.querySelector('.board');
 
-populateBoard(numberOfCards);
-
 function populateBoard(amountOfCards) {
   const generatedCard = document.getElementsByClassName('card');
   for (let i = 0; i < amountOfCards; i++) {
-    // console.log('things are happening...', dataCardName);
-    // singleCard can be the output of a function
-    // console.log(cardMap);
     board.insertAdjacentHTML('beforeend', cardMap[i].html);
     generatedCard[i].appendChild(cardMap[i].innerHTML);
     generatedCard[i].setAttribute(`${dataAttribute}`, cardMap[i].id);
     generatedCard[i].setAttribute(`id`, cardMap[i].id);
-    // generatedCard[i].setAttribute('data-card-yo', 'yo-yo');
   }
 }
+
+populateBoard(numberOfCards);
 
 //
 // matching logic...still quite unfinished
 //
+
+// events and clicks etc...
+// console.log(cardMap[3].check('meteor'));
+const cards = document.querySelectorAll('.card');
+
+function handleCardClick(event) {
+  let dataEvent = event.currentTarget.dataset;
+  let cardMethod = event;
+  console.log('the card is being clicked');
+  console.log(dataEvent);
+}
+
+// Add event listener
+cards.forEach(function(card) {
+  card.addEventListener('click', handleCardClick);
+  // console.log(card);
+
+  // card.dataset.cardName = `${returnNumberPairs(numberOfCards)}`;
+});
+
+// the promises and timings
 
 const time = 2000;
 // console.log(firstItem);
